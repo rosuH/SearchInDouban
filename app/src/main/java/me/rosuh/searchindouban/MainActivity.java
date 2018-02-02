fe8d840f91bc22db76d53effa19548d57adf6e4b
rosu <cc@rosu.me>
2018-02-02 14:52:03 +0800

    :rocket: :art: Added:     - Add inside search way Updated:     - Update loading progress info.     - Update Icon.

--- app/src/main/java/me/rosuh/searchindouban/MainActivity.java
+++ app/src/main/java/me/rosuh/searchindouban/MainActivity.java
@@ -6,76 +6,99 @@ import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import com.daasuu.ahp.AnimateHorizontalProgressBar;
 
 import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
-
-    private String userData;
-
-    private WebSettings webSettings;
-
-    private SweetAlertDialog pDialog;
-
-    private TextView textView;
-
-    private Button finishButton;
+    private boolean KEY_LAUNCH_STATUS = true;     // true 为分享启动，false 为手动启动
+    private WebView mWebView;
+    private String mUserData;
+    private WebSettings mWebSettings;
+    private EditText mEditText;
+    private ImageButton mImageButton;
+    private AnimateHorizontalProgressBar progressBar;
+    private TextView mTextView;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView)findViewById(R.id.web_view);
-        textView = (TextView)findViewById(R.id.text_view);
-        webSettings = webView.getSettings();
-        userData = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-        pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
-        pDialog.setCancelable(true);
-        finishButton = (Button)findViewById(R.id.finish_button);
-        finishButton.setOnClickListener(new View.OnClickListener() {
+        mWebView = findViewById(R.id.web_view_content_main);
+        mWebSettings = mWebView.getSettings();
+        mUserData = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        mImageButton = findViewById(R.id.image_button_search_main);
+        progressBar = findViewById(R.id.progress_bar_main);
+        mEditText = findViewById(R.id.edit_view_main);
+        mTextView = findViewById(R.id.text_view_tip_main);
+
+        progressBar.setMax(1000);
+        progressBar.setProgress(400);
+
+        mWebView.setWebViewClient(new WebViewClient());
+        if (mUserData == null) {
+            KEY_LAUNCH_STATUS = false;
+            setViewVisibility();
+        }else {
+            doSearch(mUserData);
+        }
+
+        mImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                finish();
+            public void onClick(View v) {
+                String str = mEditText.getText().toString();
+                if (!str.isEmpty()){
+                    doSearch(str);
+                }else {
+                    Toast.makeText(MainActivity.this, R.string.nothing, Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
-        webView.setWebViewClient(new WebViewClient());
-        if (userData == null) {
-            webView.setVisibility(View.GONE);
-            textView.setVisibility(View.VISIBLE);
-            finishButton.setVisibility(View.VISIBLE);
-            textView.setText("1. 选中文字\n" +
-                    "2. 长按后选中分享\n" +
-                    "3. 选中 ·查豆瓣· 进行查找\n");
-        }else {
-            webView.loadUrl("https://m.douban.com/search?query=" + userData);
-            pDialog.setTitleText("加载中...");
-            pDialog.setContentText("");
-            webSettings.setSupportMultipleWindows(true);
-            webView.setWebChromeClient(new WebChromeClient(){
-                @Override
-                public void onProgressChanged(WebView view, int progress) {
-                    if (progress <= 70) {
-                        pDialog.show();
-                    }else{
-                        pDialog.dismiss();
-                    }
-                }
-            });
+    private void doSearch(String data){
+        KEY_LAUNCH_STATUS = true;
+        setViewVisibility();
+        mWebView.loadUrl("https://m.douban.com/search?query=" + data);
+        mWebSettings.setSupportMultipleWindows(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgressWithAnim(progress*40);
+            }
+        });
+    }
+    
+    private void setViewVisibility(){
+        if (KEY_LAUNCH_STATUS){
+            // 分享启动
+            mWebView.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+
+            mEditText.setVisibility(GONE);
+            mImageButton.setVisibility(GONE);
+            mTextView.setVisibility(GONE);
+        }else if (!KEY_LAUNCH_STATUS){
+            // 启动器启动
+            mEditText.setVisibility(View.VISIBLE);
+            mImageButton.setVisibility(View.VISIBLE);
+            mTextView.setVisibility(View.VISIBLE);
+            mWebView.setVisibility(GONE);
+            progressBar.setVisibility(GONE);
         }
     }
 
@@ -87,11 +110,20 @@ public class MainActivity extends AppCompatActivity {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
                 case KeyEvent.KEYCODE_BACK:
-                    if (webView.canGoBack()) {
-                        webView.goBack();
-                    }else {
-                        finish();
+                    if (mWebView.canGoBack()){
+                        mWebView.goBack();
+                        count = 0;
+                    } else {
+                        if (count == 0){
+                            KEY_LAUNCH_STATUS = false;
+                            setViewVisibility();
+                            count++;
+                        }else {
+                            finish();
+                        }
+
                     }
+                    Log.d("KeyEvent", keyCode + "");
                     return true;
             }
         }
@@ -101,12 +133,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        webSettings.setJavaScriptEnabled(true);
+        mWebSettings.setJavaScriptEnabled(true);
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        webSettings.setJavaScriptEnabled(false);
+        mWebSettings.setJavaScriptEnabled(false);
     }
 }
